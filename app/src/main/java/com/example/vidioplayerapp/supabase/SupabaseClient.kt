9080dc2045eb7445177a6622259363f828c25f95
Azadshekhar <shekharmaurya8978@gmail.com>
2024-05-08 16:37:54 +0530

    first commit

---
+++ app/src/main/java/com/example/vidioplayerapp/supabase/SupabaseClient.kt
@@ -0,0 +1,17 @@
+package com.example.vidioplayerapp.supabase
+
+/*import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.storage.Storage*/
+
+class SupabaseClient {
+  /*  private fun getClient(){
+        val client= createSupabaseClient(
+            supabaseUrl = "https://tpiqcelaibcmarmbsywe.supabase.co",
+            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwaXFjZWxhaWJjbWFybWJzeXdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ5ODY1NDQsImV4cCI6MjAzMDU2MjU0NH0.jsJmgqRXKWvTHs9eK8rh57gpykxKCT9HFns-HrdIRAw",
+        ){
+            install(Postgrest)
+            install(Storage)
+        }
+    }*/
+}
---
+++ app/src/main/java/com/example/vidioplayerapp/supabase/SupabaseViewmodel.kt
@@ -0,0 +1,11 @@
+package com.example.vidioplayerapp.supabase
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class SupabaseViewmodel:ViewModel() {
+
+}
